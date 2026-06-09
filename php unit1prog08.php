eb47f29ca02e59e13393a448aa6d3faa1031c683
Vansh2405 <vanshmehta1555@gmail.com>
2026-06-09 11:13:31 +0530

    Update php unit1prog08.php

--- php unit1prog08.php
+++ php unit1prog08.php
@@ -6,24 +6,34 @@
     <title>Document</title>
 </head>
 <body>
-   <?php
-    error_reporting(0);
-?>
-
-    <form method="get">
-            value 1:<input type="text" name="arr[]"><br><br>
-            value 2:<input type="text" name="arr[]"><br><br>
-            value 3:<input type="text" name="arr[]"><br><br>
-
-            <input type="submit" name="submit" value="submit">
-    </form>
-
+   
 <?php
-    $arr = $_GET['arr'];
+        error_reporting(0);
+?>
+        <form method="get">
+            Enter number of values: 
+            <input type="number" name="n" required>
+            <input type="submit" name="generate" value="Generate">
+            
+        </form>
+        <form method = "get">
 
-    foreach ($arr as $value) {
-        echo $value . "<br>";
-    } 
+            <?php
+                $n = $_GET['n'];
+                for ($i = 0; $i < $n; $i++) {
+                    echo "Value " . ($i + 1) . "<input type='text' name='arr[]'><br><br>";
+                }
+
+            ?>
+
+                <input type="submit" name="submit" value="Submit">
+
+        </form>
+        <?php
+            $array = $_GET['arr'];
+             foreach ($array as $ele) {
+                echo $ele . "<br>";
+            }
 ?>
 </body>
 </html>
